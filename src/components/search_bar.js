4050ac669e4564442e2ecd9a2cc58bcef5132687
Su Tanh Thanh Tam <sutanhthanhtam@MacBook-Pro.local>
2017-08-04 18:28:19 -0400

    Add comments to better udnerstand Redux function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'; //Because it will turn the
                            //components into React.createelement
 class SearchBar extends Component {
+  //this function does this
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
   }
 
+  //this function does that
   render() {
     return (
       <div className="search-bar">
